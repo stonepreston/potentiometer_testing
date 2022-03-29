9caa267e4c82415635443988303c04d5365a4828
Stone Preston <stonepreston@users.noreply.github.com>
2022-03-28 20:58:06 -0500

    add serial code as well

--- src/main.cpp
+++ src/main.cpp
@@ -2,16 +2,12 @@
 #include <Control_Surface.h>
 #include <ResponsiveAnalogRead.h>
 
-const int ANALOG_PIN = A4;
-ResponsiveAnalogRead analog(ANALOG_PIN, true);
-
+const int ANALOG_PIN = A17;
 // Instantiate a MIDI over USB interface.
 USBMIDI_Interface midi;
 
-// PBPotentiometer pitchBendPotentiometer = {
-//         A4,        // Analog pin connected to potentiometer
-//         CHANNEL_1, // MIDI Channel 1
-// };
+ResponsiveAnalogRead analog(ANALOG_PIN, true);
+
 PitchBendSender<10> pbSender;
 
 void setup() {
@@ -25,6 +21,40 @@ void loop() {
     if(analog.hasChanged()) {
         pbSender.send(analog.getValue(), CHANNEL_1);
     }
-    
     delay(20);
 }
+
+// // define the pin you want to use
+// const int ANALOG_PIN = A17;
+
+// // make a ResponsiveAnalogRead object, pass in the pin, and either true or false depending on if you want sleep enabled
+// // enabling sleep will cause values to take less time to stop changing and potentially stop changing more abruptly,
+// //   where as disabling sleep will cause values to ease into their correct position smoothly and more accurately
+// ResponsiveAnalogRead analog(ANALOG_PIN, true);
+
+// // the next optional argument is snapMultiplier, which is set to 0.01 by default
+// // you can pass it a value from 0 to 1 that controls the amount of easing
+// // increase this to lessen the amount of easing (such as 0.1) and make the responsive values more responsive
+// // but doing so may cause more noise to seep through if sleep is not enabled
+
+// void setup() {
+//   // begin serial so we can see analog read values through the serial monitor
+//   Serial.begin(9600);
+// }
+
+// void loop() {
+//   // update the ResponsiveAnalogRead object every loop
+//   analog.update();
+
+//   Serial.print(analog.getRawValue());
+//   Serial.print("\t");
+//   Serial.print(analog.getValue());
+  
+//   // if the repsonsive value has change, print out 'changed'
+//   if(analog.hasChanged()) {
+//     Serial.print("\tchanged");
+//   }
+  
+//   Serial.println("");
+//   delay(20);
+// }
