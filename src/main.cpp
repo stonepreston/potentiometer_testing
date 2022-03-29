214e892ff3adc83b55140294f94271a8913097d9
Stone Preston <stonepreston@users.noreply.github.com>
2022-03-28 22:23:20 -0500

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,60 +1,41 @@
 #include <Arduino.h>
 #include <Control_Surface.h>
+// include the ResponsiveAnalogRead library
 #include <ResponsiveAnalogRead.h>
 
+// define the pin you want to use
 const int ANALOG_PIN = A17;
-// Instantiate a MIDI over USB interface.
-USBMIDI_Interface midi;
 
+USBDebugMIDI_Interface midi {9600};
+
+// make a ResponsiveAnalogRead object, pass in the pin, and either true or false depending on if you want sleep enabled
+// enabling sleep will cause values to take less time to stop changing and potentially stop changing more abruptly,
+//   where as disabling sleep will cause values to ease into their correct position smoothly and more accurately
 ResponsiveAnalogRead analog(ANALOG_PIN, true);
 
-PitchBendSender<10> pbSender;
+// the next optional argument is snapMultiplier, which is set to 0.01 by default
+// you can pass it a value from 0 to 1 that controls the amount of easing
+// increase this to lessen the amount of easing (such as 0.1) and make the responsive values more responsive
+// but doing so may cause more noise to seep through if sleep is not enabled
 
 void setup() {
-
-    Control_Surface.begin(); // Initialize Control Surface
+//   Control_Surface.begin(); // THIS CAUSES WERID BEHAVIOR!!! IF I COMMENT THIS OUT THINGS WORK AS EXPECTED
+//  IF I DONT COMMENT IT OUT, THE ANALOG READ IS NOISY/ONLY READS HALF THE POT VALUES
 }
 
 void loop() {
-    Control_Surface.loop(); // Update the Control Surface
-    analog.update();
-    if(analog.hasChanged()) {
-        pbSender.send(analog.getValue(), CHANNEL_1);
-    }
-    delay(20);
+  // update the ResponsiveAnalogRead object every loop
+  analog.update();
+
+  Serial.print(analog.getRawValue());
+  Serial.print("\t");
+  Serial.print(analog.getValue());
+  
+  // if the repsonsive value has change, print out 'changed'
+  if(analog.hasChanged()) {
+    Serial.print("\tchanged");
+  }
+  
+  Serial.println("");
+  delay(20);
 }
-
-// // define the pin you want to use
-// const int ANALOG_PIN = A17;
-
-// // make a ResponsiveAnalogRead object, pass in the pin, and either true or false depending on if you want sleep enabled
-// // enabling sleep will cause values to take less time to stop changing and potentially stop changing more abruptly,
-// //   where as disabling sleep will cause values to ease into their correct position smoothly and more accurately
-// ResponsiveAnalogRead analog(ANALOG_PIN, true);
-
-// // the next optional argument is snapMultiplier, which is set to 0.01 by default
-// // you can pass it a value from 0 to 1 that controls the amount of easing
-// // increase this to lessen the amount of easing (such as 0.1) and make the responsive values more responsive
-// // but doing so may cause more noise to seep through if sleep is not enabled
-
-// void setup() {
-//   // begin serial so we can see analog read values through the serial monitor
-//   Serial.begin(9600);
-// }
-
-// void loop() {
-//   // update the ResponsiveAnalogRead object every loop
-//   analog.update();
-
-//   Serial.print(analog.getRawValue());
-//   Serial.print("\t");
-//   Serial.print(analog.getValue());
-  
-//   // if the repsonsive value has change, print out 'changed'
-//   if(analog.hasChanged()) {
-//     Serial.print("\tchanged");
-//   }
-  
-//   Serial.println("");
-//   delay(20);
-// }
